c85f7a523f64cd71e25288f662d1696e756a1b34
KaranamK <kaushik.karanam@l-p-a.com>
2025-08-26 15:37:53 +0200

    update environment.prod.ts

--- angular-app-4/src/environments/environment.prod.ts
+++ angular-app-4/src/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiUrl: 'https://api.138.2.131.203.nip.io/' //'http://api.app-4.local/' //'http://backend:80' //'http://localhost:5000/'
+    apiUrl: 'http://api-service:80/' //'https://api.138.2.131.203.nip.io/' //'http://api.app-4.local/' //'http://backend:80' //'http://localhost:5000/'
 }
