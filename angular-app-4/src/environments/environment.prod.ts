fe3dfae203e72d1c0df5ecbe3cf5e7af63b09e33
KaranamK <kaushik.karanam@l-p-a.com>
2025-09-02 08:32:05 +0200

    making changes to expose backend-service

--- angular-app-4/src/environments/environment.prod.ts
+++ angular-app-4/src/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiUrl: 'https://api.138.2.131.203.nip.io/' //'http://api-service:80/' //'http://10.43.249.57:80/'   //'http://api.app-4.local/' //'http://backend:80' //'http://localhost:5000/'
+    apiUrl: 'http://api.138.2.131.203.nip.io/' //'http://api-service:80/' //'http://10.43.249.57:80/'   //'http://api.app-4.local/' //'http://backend:80' //'http://localhost:5000/'
 }
