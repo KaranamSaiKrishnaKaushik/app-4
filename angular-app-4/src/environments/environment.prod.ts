0d537b51bd2ae46b26c52eaa93e1d21743727671
KaranamK <kaushik.karanam@l-p-a.com>
2025-09-02 16:39:02 +0200

    Update environment file of angular

--- angular-app-4/src/environments/environment.prod.ts
+++ angular-app-4/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+    production: true,
     apiUrl: 'http://api.138.2.131.203.nip.io/' //'http://api-service:80/' //'http://10.43.249.57:80/'   //'http://api.app-4.local/' //'http://backend:80' //'http://localhost:5000/'
 }
